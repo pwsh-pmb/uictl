35f52e2dc84af4a3f683d64bfb86a5a2593bef50
mk-pmb <mk-pmb@users.noreply.github.com>
2026-07-28 10:15:49 +0200

    Add Get-CartesianProduct.

--- lib/util.array.ps1
+++ lib/util.array.ps1
@@ -6,6 +6,46 @@ function Join-AsStrings([string]$Glue=', ') {
 }
 
 
+function ForEach-ObjectAsJson {
+  $input | ForEach-Object { $_ | ConvertTo-Json -Compress }
+}
+
+
+function Get-CartesianProduct($sets) {
+  $n = $sets.Length
+  if ($n -lt 1) { return @() }
+  $a = $sets[0]
+  if ($n -eq 1) { return $a }
+  $b = $sets[1]
+  if ($n -ge 3) { $b = Get-CartesianProduct @($sets[1..($n-1)]) }
+  foreach ($x in $a) { foreach ($y in $b) { ,@($x; $y) } }
+}
+
+
+function Get-ReverseCartesianProduct($sets) {
+  $n = $sets.Length
+  if ($n -lt 1) { return @() }
+  $a = $sets[0]
+  if ($n -eq 1) { return $a }
+  if ($n -ge 3) { $a = Get-ReverseCartesianProduct @($sets[0..($n-2)]) }
+  $b = $sets[$n-1]
+  foreach ($y in $b) { foreach ($x in $a) { ,@($x; $y) } }
+}
+
+
+function Demonstrate-CartesianProductAndReverse {
+  'RGB:'
+  Get-CartesianProduct @('r', 'R'), @('g', 'G'), @('b', 'B') |
+    ForEach-ObjectAsJson
+  'BGR:'
+  Get-ReverseCartesianProduct @('r', 'R'), @('g', 'G'), @('b', 'B') |
+    ForEach-ObjectAsJson
+}
+
+
+# Demonstrate-CartesianProductAndReverse
+
+
 
 
 
